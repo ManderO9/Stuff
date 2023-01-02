bd00ea7cabb7f2df9353c9cef30dc81940d5f56a
ManderO9 <hossem.chetoui@gmail.com>
2023-01-02 12:15:20 +0100

    testing if routes work

---
+++ Stuff/ApplicationRoutes.cs
@@ -0,0 +1,10 @@
+namespace Stuff;
+
+public class ApplicationRoutes
+{
+    // TODO: change this before publishing
+    public const string RoutePrefix = "/Stuff";//"";
+
+    public const string HomePage = "/";
+    public const string ProductDetailsPage = "/ProductDetails";
+}
